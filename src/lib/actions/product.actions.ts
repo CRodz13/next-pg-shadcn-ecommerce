770890a51b4ef762ccf427185620eff746f7f780
crodz13 <rodcee13@gmail.com>
2024-07-02 22:36:44 -0400

    product details page

--- src/lib/actions/product.actions.ts
+++ src/lib/actions/product.actions.ts
@@ -4,6 +4,7 @@ import { desc } from 'drizzle-orm'
 
 import db from '@/db/drizzle'
 import { products } from '@/db/schema'
+import { eq } from 'drizzle-orm/sql'
 
 export async function getLatestProducts() {
     const data = await db.query.products.findMany({
@@ -12,3 +13,9 @@ export async function getLatestProducts() {
     })
     return data
 }
+
+export async function getProductBySlug(slug: string) {
+    return await db.query.products.findFirst({
+      where: eq(products.slug, slug),
+    })
+  }
