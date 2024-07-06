299228f0b50be77cdcc4ae6ffb02efea0d3ca050
crodz13 <rodcee13@gmail.com>
2024-07-06 15:27:55 -0400

    list users

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -10,8 +10,9 @@ import db from '@/db/drizzle'
 import { users } from '@/db/schema'
 import { ShippingAddress } from '@/types'
 import { revalidatePath } from 'next/cache'
-import { eq } from 'drizzle-orm'
+import { count, desc, eq } from 'drizzle-orm'
 import { z } from 'zod'
+import { PAGE_SIZE } from '../constants'
 
 // CREATE
 export async function signUp(prevState: unknown, formData: FormData) {
@@ -75,6 +76,21 @@ export async function getUserById(userId: string) {
     return user
 }
 
+// DELETE
+export async function deleteUser(id: string) {
+    try {
+      await db.delete(users).where(eq(users.id, id))
+      revalidatePath('/admin/users')
+      return {
+        success: true,
+        message: 'User deleted successfully',
+      }
+    } catch (error) {
+      return { success: false, message: formatError(error) }
+    }
+  }
+  
+  // UPDATE
 export async function updateUserAddress(data: ShippingAddress) {
     try {
         const session = await auth()
@@ -148,3 +164,23 @@ export async function updateProfile(user: { name: string; email: string }) {
 export const SignOut = async () => {
     await signOut()
 }
+
+export async function getAllUsers({
+    limit = PAGE_SIZE,
+    page,
+}: {
+    limit?: number
+    page: number
+}) {
+    const data = await db.query.users.findMany({
+        orderBy: [desc(users.createdAt)],
+        limit,
+        offset: (page - 1) * limit,
+    })
+    const dataCount = await db.select({ count: count() }).from(users)
+    return {
+        data,
+        totalPages: Math.ceil(dataCount[0].count / limit),
+    }
+}
+
